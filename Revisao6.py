627a55f24434b587e3fb4089c5527425562f194a
joaoxavier23 <nseimeesqueci@gmail.com>
2023-05-06 23:48:33 -0300

    Revisando lógica

---
+++ Revisao6.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número: "))
+while numero==10:
+    numero = int(input("DIGITE UM NUMERO DIFERENTE DE 10: "))
+if numero >10:
+    print("É MAIOR QUE 10")
+else:
+    print("NÃO É MAIOR QUE 10")
