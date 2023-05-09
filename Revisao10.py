c5c83bd75bd7e816fd5d180c91328dc83873ae19
João Victor Xavier <106491893+joaoxavier23@users.noreply.github.com>
2023-05-09 13:30:43 -0300

    Revisando lógica

---
+++ Revisao10.py
@@ -0,0 +1,7 @@
+idade=int(input("Digite sua idade: "))
+meses=int(input("Digite quantos meses: "))
+dias=int(input("Digite quantos dias: "))
+
+idade_dias=idade*365 + meses*30 + dias
+
+print("Você tem",idade_dias,"Dias")
